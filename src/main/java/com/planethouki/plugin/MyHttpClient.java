040ae54732de6635e06d4861cab28e6de8a19be6
planethouki <syn_tok@hotmail.co.jp>
2017-12-02 11:18:30 +0900

    Http Get Coding

--- src/main/java/com/planethouki/plugin/MyHttpClient.java
+++ src/main/java/com/planethouki/plugin/MyHttpClient.java
@@ -1,10 +1,36 @@
 package com.planethouki.plugin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
 public class MyHttpClient {
     public static String executeGet(String argurl) {
     	// TODO Coding
-    	String rawResponse = "{\"code\":6,\"type\":4,\"message\":\"status\"}";
-		return rawResponse;
+    	URL url;
+    	HttpURLConnection con;
+    	StringBuilder builder = new StringBuilder();;
+    	try {
+			url = new URL(argurl);
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.connect();
+	    	String line;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			while ((line = reader.readLine()) != null) {
+				builder.append(line);
+			}
+			reader.close();
+			con.disconnect();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	//String rawResponse = "{\"code\":6,\"type\":4,\"message\":\"status\"}";
+		return builder.toString();
     }
 
     public static String executePost(String argurl, String argpost) {
