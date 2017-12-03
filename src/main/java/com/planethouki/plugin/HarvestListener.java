d52ac2b782a1394503549da0d6f0a6359b9b28bb
planethouki <syn_tok@hotmail.co.jp>
2017-12-03 14:43:34 +0900

    Harvest Transaction

--- src/main/java/com/planethouki/plugin/HarvestListener.java
+++ src/main/java/com/planethouki/plugin/HarvestListener.java
@@ -1,9 +1,12 @@
 package com.planethouki.plugin;
 
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Random;
+import java.util.TimeZone;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -45,10 +48,30 @@ public class HarvestListener implements Listener {
 		}
 
 		
-		// TODO transaction
-		// TODO get player's wallet
+		Player player = event.getPlayer();
+		String palyerAddress = plugin.getAddressConfig().getString(player.getName());
+		if (palyerAddress == null) {
+			player.sendMessage("your address is not registered yet");
+			return;
+		}
 		
-		// TODO message
-		event.getPlayer().sendMessage("Harvest!");
+		String transactionURL = "http://localhost:7890/transaction/prepare-announce";
+		String transactionData = "";
+		Calendar nemEpoch = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+		nemEpoch.set(2015, 2, 29, 0, 6, 25);
+		Calendar now = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+		String timeStamp = Long.toString((now.getTimeInMillis() - nemEpoch.getTimeInMillis())/1000);
+		String amount = "50000";
+		String fee = "50000";
+		String recipient = palyerAddress.replaceAll("-", "");
+		String deadline = Long.toString(((now.getTimeInMillis() - nemEpoch.getTimeInMillis())/1000)+3600);
+		String signer = plugin.getConfig().getString("ServerPublicKey");
+		String privateKey = plugin.getConfig().getString("ServerPrivateKey");
+		transactionData = String.format("{\"transaction\":{\"timeStamp\":%s,\"amount\":%s,\"fee\":%s,\"recipient\":\"%s\",\"type\":257,\"deadline\":%s,\"message\":{\"payload\":\"\",\"type\":1},\"version\":-1744830463,\"signer\":\"%s\"},\"privateKey\":\"%s\"}",timeStamp, amount, fee, recipient, deadline, signer, privateKey);
+		plugin.getLogger().info(transactionURL);
+		plugin.getLogger().info(transactionData);
+		plugin.getLogger().info(MyHttpClient.executePost(transactionURL, transactionData));
+		
+		event.getPlayer().sendMessage("You Got Harvest!");
 	}
 }
